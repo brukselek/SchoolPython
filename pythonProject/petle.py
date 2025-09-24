46f2c4f5a0c0b7a4dfe8e7d620953690b3777346
brukselek <bartoszadamus55@gmail.com>
2025-09-24 10:19:43 +0200

    -Commiting whole repo

--- pythonProject/petle.py
+++ pythonProject/petle.py
@@ -2,13 +2,11 @@ def suma():
     suma = 0;
     podanaLiczba = 0;
     for i in range(1,11):
-        while True:
-            try:
-                podanaLiczba = int(input("Wprowadz liczbe: >> "))
-                suma += podanaLiczba
-                break
-            except ValueError:
-                print("Blednie podana dana. Sprobuj ponownie: ")
+        try:
+            podanaLiczba = int(input("Wprowadz liczbe: >> "))
+            suma += podanaLiczba
+        except:
+            print("Blednie podana dana. Sprobuj ponownie: ")
     print("Suma podanych liczbe: ", suma)
 
 suma()
