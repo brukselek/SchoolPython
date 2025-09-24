6c7f144cdbdc3e74c6cfd96005000f8bbe384a1f
brukselek <bartoszadamus55@gmail.com>
2025-09-24 10:16:12 +0200

    Commiting some files

--- pythonProject/sredniaOcen.py
+++ pythonProject/sredniaOcen.py
@@ -1,17 +1,38 @@
-def ocenyZeSprawdzianu():
-    grades = []
+def main():
+    score = []
     for i in range(5):
-        ocena = input(f"Podaj ocene z {i+1} sprawdzianu: >> ")
-        if(int(ocena) >= 1 and int(ocena) <= 6 ):
-            grades.append(int(ocena))
+        wynik = input(f"Podaj wynik z {i+1} sprawdzianu: >> ")
+        if(int(wynik) >= 0 and int(wynik) <= 100 ):
+            score.append(int(wynik))
         else:
-            print("Blednie podana ocena, nie zostanie uwzgledniona")
-    return grades
+            print("Blednie podany wynik, nie zostanie uwzgledniona")
+    return score
+
+def determineGrade(wynik):
+    if(wynik > 90 and wynik <= 100):
+        return 6
+    elif(wynik > 80 and wynik <= 90):
+        return 5
+    elif(wynik > 70 and wynik <= 89):
+        return 4
+    elif(wynik > 70 and wynik <= 79):
+        return 3
+    elif(wynik >= 60 and wynik <= 69):
+        return 2
+    else:
+        return 1
 
 def calcAverage(grades):
     if grades:
         average = sum(grades) / len(grades)
         print(f"Srednia sprawdzianu: {average:.2f}")
 
-oceny = ocenyZeSprawdzianu()
+wyniki = main()
+oceny = []
+
+for wynik in wyniki:
+    ocena = determineGrade(wynik)
+    oceny.append(ocena)
+    print(f"Wynik: {wynik} => Ocena: {ocena}")
+
 calcAverage(oceny)
